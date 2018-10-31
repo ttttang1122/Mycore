d9559f2ae25bd0e6aff3f1ed7903ed767a1df7d6
随风一生 <544297534@qq.com>
2018-10-31 16:33:40 +0800

    报表更新

--- MyCore/Controllers/Store/GoodsStoreController.cs
+++ MyCore/Controllers/Store/GoodsStoreController.cs
@@ -11,6 +11,8 @@ using MyCore.Models;
 using Microsoft.AspNetCore.Http;
 using System.Text;
 using Microsoft.EntityFrameworkCore;
+using System.Linq.Expressions;
+using MyCore.Models.Search;
 
 namespace MyCore.Controllers.Store
 {
@@ -27,69 +29,119 @@ namespace MyCore.Controllers.Store
             return View();
         }
 
-        public async Task<IActionResult> GoodsStoreList(string sidx, string sord, int page, int rows, string StrSearchType, string StrSearch)
+        public async Task<IActionResult> GoodsStoreList(string sidx, string sord, int page, int rows, Search_GoodStoreBill Search )
         {
-            IQueryable<GoodsStore> bills = conn.GoodsStore.Where(b=>b.Num>0);
+            Expression<Func<GoodsStore, bool>> predicate = ExpressionBuilder.True<GoodsStore>();
+            predicate = predicate.And(b => b.Num > 0);
 
-            if (!string.IsNullOrWhiteSpace(StrSearchType))
+            if (Search.StartDate != null)
             {
-                if (!string.IsNullOrWhiteSpace(StrSearch))
-                {
-                    switch (StrSearchType)
-                    {
-                        case "0":
-                            bills = bills.Where(b => b.StoreName.Contains(StrSearch));
-                            break;
-                        case "1":
-                            bills = bills.Where(b => b.GoodID.Contains(StrSearch));
-
-                            break;
-                        case "2":
-                            bills = bills.Where(b => b.GoodName.Contains(StrSearch));
-
-                            break;
-                        default:
-
-                            break;
-                    }
-
-                }
+                predicate = predicate.And(b => b.yxqDate >= Search.StartDate);
             }
+            if (Search.EndDate != null)
+            {
+                predicate = predicate.And(b => b.yxqDate <= Search.EndDate);
+            }
+            if (!string.IsNullOrWhiteSpace(Search.StoreName))
+            {
+                predicate = predicate.And(b => b.StoreName.Contains(Search.StoreName));
+            }
+            if (!string.IsNullOrWhiteSpace(Search.GoodID))
+            {
+                predicate = predicate.And(b => b.GoodID.Contains(Search.GoodID));
+            }
+            if (!string.IsNullOrWhiteSpace(Search.GoodName))
+            {
+                predicate = predicate.And(b => b.GoodName.Contains(Search.GoodName));
+            }
+            if (!string.IsNullOrWhiteSpace(Search.DW))
+            {
+                predicate = predicate.And(b => b.DW.Contains(Search.DW));
+            }
+            if (!string.IsNullOrWhiteSpace(Search.GGType))
+            {
+                predicate = predicate.And(b => b.GGType.Contains(Search.GGType));
+            }
+            if (!string.IsNullOrWhiteSpace(Search.ModelType))
+            {
+                predicate = predicate.And(b => b.ModelType.Contains(Search.ModelType));
+            }
+            if (!string.IsNullOrWhiteSpace(Search.SCCJ))
+            {
+                predicate = predicate.And(b => b.SCCJ.Contains(Search.SCCJ));
+            }
+            if (!string.IsNullOrWhiteSpace(Search.SCPH))
+            {
+                predicate = predicate.And(b => b.SCPH.Contains(Search.SCPH));
+            }
+            if (!string.IsNullOrWhiteSpace(Search.MJPH))
+            {
+                predicate = predicate.And(b => b.MJPH.Contains(Search.MJPH));
+            }
+
+            IQueryable<GoodsStore> bills = conn.GoodsStore.Where(predicate);
+
+          
 
             var lists = await bills.ToListAsync();
             return lists.GetJson<GoodsStore>(sidx, sord, page, rows, SysTool.GetPropertyNameArray<GoodsStore>());
         }
 
         [HttpPost]
-        public async Task<IActionResult> GetFile(string StrSearchType, string StrSearch)
+        public async Task<IActionResult> GetFile(string JsonSearch)
         {
+            //json字符串转模型
+            var Search = SysTool.JsonToModel<Search_GoodStoreBill>(JsonSearch);
 
-            IQueryable<GoodsStore> bills = conn.GoodsStore.Where(b => b.Num > 0);
+            Expression<Func<GoodsStore, bool>> predicate = ExpressionBuilder.True<GoodsStore>();
+            predicate = predicate.And(b => b.Num > 0);
 
-            if (!string.IsNullOrWhiteSpace(StrSearchType))
+            if (Search.StartDate != null)
             {
-                if (!string.IsNullOrWhiteSpace(StrSearch))
-                {
-                    switch (StrSearchType)
-                    {
-                        case "0":
-                            bills = bills.Where(b => b.StoreName.Contains(StrSearch));
-                            break;
-                        case "1":
-                            bills = bills.Where(b => b.GoodID.Contains(StrSearch));
-
-                            break;
-                        case "2":
-                            bills = bills.Where(b => b.GoodName.Contains(StrSearch));
-
-                            break;
-                        default:
-
-                            break;
-                    }
-
-                }
+                predicate = predicate.And(b => b.yxqDate >= Search.StartDate);
             }
+            if (Search.EndDate != null)
+            {
+                predicate = predicate.And(b => b.yxqDate <= Search.EndDate);
+            }
+            if (!string.IsNullOrWhiteSpace(Search.StoreName))
+            {
+                predicate = predicate.And(b => b.StoreName.Contains(Search.StoreName));
+            }
+            if (!string.IsNullOrWhiteSpace(Search.GoodID))
+            {
+                predicate = predicate.And(b => b.GoodID.Contains(Search.GoodID));
+            }
+            if (!string.IsNullOrWhiteSpace(Search.GoodName))
+            {
+                predicate = predicate.And(b => b.GoodName.Contains(Search.GoodName));
+            }
+            if (!string.IsNullOrWhiteSpace(Search.DW))
+            {
+                predicate = predicate.And(b => b.DW.Contains(Search.DW));
+            }
+            if (!string.IsNullOrWhiteSpace(Search.GGType))
+            {
+                predicate = predicate.And(b => b.GGType.Contains(Search.GGType));
+            }
+            if (!string.IsNullOrWhiteSpace(Search.ModelType))
+            {
+                predicate = predicate.And(b => b.ModelType.Contains(Search.ModelType));
+            }
+            if (!string.IsNullOrWhiteSpace(Search.SCCJ))
+            {
+                predicate = predicate.And(b => b.SCCJ.Contains(Search.SCCJ));
+            }
+            if (!string.IsNullOrWhiteSpace(Search.SCPH))
+            {
+                predicate = predicate.And(b => b.SCPH.Contains(Search.SCPH));
+            }
+            if (!string.IsNullOrWhiteSpace(Search.MJPH))
+            {
+                predicate = predicate.And(b => b.MJPH.Contains(Search.MJPH));
+            }
+
+            IQueryable<GoodsStore> bills = conn.GoodsStore.Where(predicate);
 
             var lists = await bills.ToListAsync();
 
@@ -99,6 +151,7 @@ namespace MyCore.Controllers.Store
             var fileName = "库存信息" + DateTime.Now.ToString("yyyy-MM-dd") + ".xls";
 
             return File(buffer, "application/vnd.ms-excel", fileName);
+
         }
     }
 }
